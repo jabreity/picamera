f20ca20b624e6fe7ea6c1b978fe9e950c372a1ba
Jason Breitwieser <83088533+jabreity@users.noreply.github.com>
2023-11-17 17:07:52 -0500

    Update picamera2still.py
    Removed 60 count

--- picamera2still.py
+++ picamera2still.py
@@ -5,14 +5,13 @@ picam2 = Picamera2()
 camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)})
 picam2.configure(camera_config)
 picam2.start()
-for i in range(1, 60):
-	second = None
-	while True:
-		if datetime.now().second != second:
-			timestamp = str(datetime.now())
-			timestamp = timestamp.replace(":", "-")
-			timestamp = timestamp.replace(" ", "_")
-			timestamp += '.jpg'
-			picam2.capture_file(timestamp)
-			second = datetime.now().second
+second = None
+while True:
+	if datetime.now().second != second:
+		timestamp = str(datetime.now())
+		timestamp = timestamp.replace(":", "-")
+		timestamp = timestamp.replace(" ", "_")
+		timestamp += '.jpg'
+		picam2.capture_file(timestamp)
+		second = datetime.now().second
 picam2.stop()
