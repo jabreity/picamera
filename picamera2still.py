52802a22b27567b6d5769e4a4dbe4b3e769345ed
Jason Breitwieser <83088533+jabreity@users.noreply.github.com>
2023-11-17 16:58:29 -0500

    First Commit
    Feature complete.  Output is a .jpg in the folder from which it was launched.

---
+++ picamera2still.py
@@ -0,0 +1,19 @@
+from picamera2 import Picamera2
+from datetime import datetime
+import time
+picam2 = Picamera2()
+camera_config = picam2.create_still_configuration(main={"size": (1920, 1080)})
+picam2.configure(camera_config)
+picam2.start()
+for i in range(1, 60):
+	second = None
+	while True:
+		if datetime.now().second != second:
+			print(f'Do something {datetime.now()}')
+			timestamp = str(datetime.now())
+			timestamp = timestamp.replace(":", "-")
+			timestamp = timestamp.replace(" ", "_")
+			timestamp += '.jpg'
+			picam2.capture_file(timestamp)
+			second = datetime.now().second
+picam2.stop()
